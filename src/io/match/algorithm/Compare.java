470912de629b3608de6a73689d87deb4d720f289
minhhoangtcu <minh.hoang@tcu.edu>
2016-01-29 12:58:32 -0600

    Initialize the compare class

---
+++ src/io/match/algorithm/Compare.java
@@ -0,0 +1,49 @@
+package io.match.algorithm;
+
+import java.util.Iterator;
+
+import io.match.datastructure.Person;
+import io.match.datastructure.attributes.Attribute;
+import io.match.datastructure.attributes.GeneralAttribute;
+import io.match.datastructure.attributes.MultipleAttribute;
+import io.match.datastructure.attributes.ScaleAttribute;
+
+public class Compare {
+
+	/**
+	 * Get the percentage matching from 2 people. This algorithm does not care about whether the person is a student or faculty.
+	 * 
+	 * @param first the first person
+	 * @param second the second person
+	 * @return
+	 */
+	public static int getMatch(Person first, Person second) {
+		
+		if (first.getAttributes().size() != second.getAttributes().size())
+			throw new IllegalArgumentException("Numbers of attributes from 2 arguments do not match!");
+		
+		Iterator<Attribute> firstIterator = first.getAttributes().iterator();
+		Iterator<Attribute> secondIterator = second.getAttributes().iterator();
+		 
+		
+		for (int i = 0; i < first.getAttributes().size(); i++) {
+			
+			Attribute firstAttr = firstIterator.next();
+			Attribute secondAttr = secondIterator.next();
+			
+			if (firstAttr instanceof GeneralAttribute && secondAttr instanceof GeneralAttribute) {
+				
+			}
+			else if (firstAttr instanceof MultipleAttribute && secondAttr instanceof MultipleAttribute) {
+				
+			}
+			else if (firstAttr instanceof ScaleAttribute && secondAttr instanceof ScaleAttribute) {
+				
+			}
+			
+		}
+		
+		return 0;
+	}
+
+}
