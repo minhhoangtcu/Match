2d46405dd8438a61f9aa333c3b98567809821fe7
Minh Hoang TCU <minh.hoang@tcu.edu>
2016-03-10 13:27:06 -0600

    Changed the text for displaying Attributes.

--- src/io/match/gui/left/TableRow.java
+++ src/io/match/gui/left/TableRow.java
@@ -4,6 +4,7 @@ import javafx.beans.property.SimpleStringProperty;
 
 public class TableRow {
 	
+	// Required names. These variables cannot be changed without the changes in TablePopulator.
 	private final SimpleStringProperty name;
 	private final SimpleStringProperty value;
 	
