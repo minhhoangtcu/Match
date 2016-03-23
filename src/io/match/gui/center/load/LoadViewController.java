a8d1759afb5b3518ce3f1a337cce45dd9438f243
Minh Hoang TCU <minh.hoang@tcu.edu>
2016-03-23 14:29:30 -0500

    Added label responding to loaded file

--- src/io/match/gui/center/load/LoadViewController.java
+++ src/io/match/gui/center/load/LoadViewController.java
@@ -1,81 +1,88 @@
 package io.match.gui.center.load;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-
 import io.match.Model;
 import io.match.helper.LoadFile;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class LoadViewController {
-	
+
 	private Model model;
-	private String studentsURL = "", 
-			fixedStudentsURL = "",
-			facultiesURL = "", 
-			fixedFacultiesURL = "";
-	
+	private String studentsURL, fixedStudentsURL, facultiesURL, fixedFacultiesURL;
+
 	@FXML
 	Label infoLoadStudentData;
-	
+
 	@FXML
 	Label infoLoadStudentFA;
-	
+
 	@FXML
 	Label infoLoadFalcutyData;
-	
+
 	@FXML
 	Label infoLoadFalcutyFA;
-	
+
 	public void setModel(Model model) {
 		this.model = model;
 	}
-	
+
 	public void loadStudents() {
 		System.out.println("From loadStudents: LoadViewController");
-		try {
-			studentsURL = LoadFile.getURL();
-		} catch (FileNotFoundException e) {
-			System.out.println("File not found");
+		File data = LoadFile.getFile();
+		if (data == null)
+			alertFileNotFound();
+		else {
+			studentsURL = data.getAbsolutePath();
+			infoLoadStudentData.setText(data.getName());
+			tryPopulatePersons();
 		}
-		tryPopulatePersons();
-	}
-	
-	public void loadFixedStudents() {
-		System.out.println("From loadFixedStudents: LoadViewController");
-		try {
-			fixedStudentsURL = LoadFile.getURL();
-		} catch (FileNotFoundException e) {
-			System.out.println("File not found");
-		}
-		tryPopulatePersons();
-	}
-	
-	public void loadFaculties() {
-		System.out.println("From loadFaculties: LoadViewController");
-		try {
-			facultiesURL = LoadFile.getURL();
-		} catch (FileNotFoundException e) {
-			System.out.println("File not found");
-		}
-		tryPopulatePersons();
-	}
-	
-	public void loadFixedFaculties() {
-		System.out.println("From loadFixedFaculties: LoadViewController");
-		try {
-			fixedFacultiesURL = LoadFile.getURL();
-		} catch (FileNotFoundException e) {
-			System.out.println("File not found");
-		}
-		tryPopulatePersons();
 	}
 
+	public void loadFixedStudents() throws FileNotFoundException {
+		System.out.println("From loadFixedStudents: LoadViewController");
+		File data = LoadFile.getFile();
+		if (data == null)
+			alertFileNotFound();
+		else {
+			fixedStudentsURL = data.getAbsolutePath();
+			infoLoadStudentFA.setText(data.getName());
+			tryPopulatePersons();
+		}
+	}
+
+	public void loadFaculties() throws FileNotFoundException {
+		System.out.println("From loadFaculties: LoadViewController");
+		File data = LoadFile.getFile();
+		if (data == null)
+			alertFileNotFound();
+		else {
+			facultiesURL = data.getAbsolutePath();
+			infoLoadFalcutyData.setText(data.getName());
+			tryPopulatePersons();
+		}
+	}
+
+	public void loadFixedFaculties() throws FileNotFoundException {
+		System.out.println("From loadFixedFaculties: LoadViewController");
+		File data = LoadFile.getFile();
+		if (data == null)
+			alertFileNotFound();
+		else {
+			fixedFacultiesURL = data.getAbsolutePath();
+			infoLoadFalcutyFA.setText(data.getName());
+			tryPopulatePersons();
+		}
+	}
+
+	private void alertFileNotFound() {
+		// SHOW POPUP WINDOW ALERTING USER
+	}
+	
 	private void tryPopulatePersons() {
-		if (!studentsURL.equals("")
-				&& !fixedStudentsURL.equals("")
-				&& !facultiesURL.equals("")
+		if (!studentsURL.equals("") && !fixedStudentsURL.equals("") && !facultiesURL.equals("")
 				&& !fixedFacultiesURL.equals("")) {
 			try {
 				model.loadStudents(studentsURL, fixedStudentsURL);
@@ -87,7 +94,8 @@ public class LoadViewController {
 				e.printStackTrace();
 			}
 		} else {
-			System.out.println(studentsURL + "\n" + fixedStudentsURL + "\n" + facultiesURL + "\n" + fixedFacultiesURL + "\n");
+			System.out.println(
+					studentsURL + "\n" + fixedStudentsURL + "\n" + facultiesURL + "\n" + fixedFacultiesURL + "\n");
 		}
 	}
 }
--- src/io/match/helper/LoadFile.java
+++ src/io/match/helper/LoadFile.java
@@ -1,25 +1,11 @@
 package io.match.helper;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-
 import javafx.stage.FileChooser;
 
 public class LoadFile {
-	private static String fileURL;
-	private static File file;
-	private static final String sampleURLFromQ = "data/";
 	
-	public static String getURL()throws FileNotFoundException {
-		file = getFile();
-		if (file == null) {
-			throw new FileNotFoundException("File Not Found");
-		} else {
-			return file.getAbsolutePath();
-		}
-		
-		
-	}
+	private static final String sampleURLFromQ = "data/";
 	
 	public static File getFile() {
 		FileChooser chooser = new FileChooser();
